1edde0c69f1118ecab11117785f94df43b910847
darwich6 <adarwich2010@gmail.com>
2024-08-16 11:32:11 -0500

    fix: unhide sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 href={auth.status === "AUTHED" ? "/dashboard" : "/sign-in"}
-                className="hidden rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
               </Link>
