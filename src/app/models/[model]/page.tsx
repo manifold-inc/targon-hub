40dbf1c67694abfd482584f3bb168f3a3dca87bd
darwich6 <adarwich2010@gmail.com>
2024-10-02 15:22:16 -0500

    feat: individual model page

---
+++ src/app/models/[model]/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useAuth } from "@/app/_components/providers";
+import { useParams } from "next/navigation";
+
+export default function Page() {
+  const auth = useAuth();
+  const params = useParams();
+  const model = params.model as string;
+
+  return (
+    <div>
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-50 sm:text-6xl">
+              Welcome {auth.user || ""} to model page {model}
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-200">
+              Powered by the bittensor ecosystem on subnet 4. Cheaper, better,
+              faster.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
