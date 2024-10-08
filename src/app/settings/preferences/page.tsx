1c73c217656988ec0901087ee435c39f3a273d85
Carro <robert@manifold.inc>
2024-10-08 14:59:13 -0500

    settings sidebar cleanup

--- src/app/settings/preferences/page.tsx
+++ src/app/settings/preferences/page.tsx
@@ -31,41 +31,41 @@ export default function Page() {
                 }`} />
               </button>
             </div>
-            <p className="text-sm text-gray-400 mt-2 dark:text-gray-500">
+            <p className="text-sm text-black mt-2 dark:text-gray-500">
               Alert notifications will be sent to alex@manifold.inc
             </p>
           </section>
           
           <section className="mb-10">
             <h2 className="text-2xl font-semibold mb-4">Account</h2>
-            <p className="text-sm text-gray-400 mb-4 dark:text-gray-500">
+            <p className="text-sm text-black mb-4 dark:text-gray-500">
               Manage your login credentials, security settings, or delete your account.
             </p>
-            <button className="bg-manifold-pink2 hover:bg-manifold-pink text-grey-900 px-6 py-2 rounded-lg transition duration-200">
+            <button className="bg-manifold-pink2 text-white dark:text-gray-100 hover:bg-manifold-pink px-6 py-2 rounded-lg transition duration-200">
               Manage Account
             </button>
           </section>
           
           <section className="mb-10">
             <h2 className="text-2xl font-semibold mb-4">Ignored Providers</h2>
-            <p className="text-sm text-gray-400 mb-4 dark:text-gray-500 ">
+            <p className="text-sm text-black mb-4 dark:text-gray-500 ">
               Select the providers you want to exclude from serving your requests.
             </p>
-            <select className="bg-manifold-grey1-800 text-white p-3 rounded-lg w-full border border-gray-700 focus:outline-none focus:ring-2 focus:ring-manifold-pink dark:bg-manifold-grey1-800 dark:border-gray-600">
+            <select className="dark:bg-manifold-grey1-800 text-black dark:text-white p-3 rounded-lg w-full border border-gray-700 focus:outline-none focus:ring-2 focus:ring-manifold-pink dark:bg-manifold-grey1-800 dark:border-gray-600">
               <option>Select a provider</option>
             </select>
-            <p className="text-sm text-gray-400 mt-2 dark:text-gray-500">
+            <p className="text-sm text-black mt-2 dark:text-gray-500">
               No providers are ignored.
             </p>
           </section>
           
           <section>
             <h2 className="text-2xl font-semibold mb-4">Default Model</h2>
-            <p className="text-sm text-gray-400 mb-4 dark:text-gray-500">
+            <p className="text-sm text-black mb-4 dark:text-gray-500">
               Apps will use this model by default, but they may override it if they choose to do so. This model will also be used as your default fallback model.
             </p>
-            <a href="#" className="text-manifold-pink hover:underline">Click here</a> to browse available models and prices.
-            <select className="bg-manifold-grey1-800 text-white p-3 rounded-lg w-full mt-4 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-manifold-pink dark:bg-manifold-grey1-800 dark:border-gray-600">
+            <a href="#" className="dark:text-manifold-pink text-manifold-green hover:underline">Click here</a> to browse available models and prices.
+            <select className="dark:bg-manifold-grey1-800 text-black dark:text-white p-3 rounded-lg w-full mt-4 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-manifold-pink dark:bg-manifold-grey1-800 dark:border-gray-600">
               <option>Select a model</option>
             </select>
           </section>
