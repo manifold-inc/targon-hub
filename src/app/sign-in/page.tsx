56eb483eff1fa8de7e089603d625fc762e2aee86
josh <josh@mjbrowns.com>
2024-11-20 14:38:54 -0800

    fix: sign in font

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -39,7 +39,7 @@ export default function Page() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className="mt-6 text-center text-2xl font-light leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
         </div>
