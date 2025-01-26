8f90d3bb349a365443bba2656546dfd1b8d45a40
darwich6 <adarwich2010@gmail.com>
2025-01-26 11:09:07 -0600

    fix: search bar api call

--- src/app/_components/landing/SearchBar.tsx
+++ src/app/_components/landing/SearchBar.tsx
@@ -16,13 +16,13 @@ import { reactClient } from "@/trpc/react";
 export default function SearchBar() {
   const router = useRouter();
   const [query, setQuery] = useState("");
-  const models = reactClient.model.getModels.useQuery(
+  const models = reactClient.model.getActiveSearchModels.useQuery(
     { name: query },
     { keepPreviousData: true },
   );
 
   const groupedModels =
-    models.data?.items.reduce(
+    models.data?.reduce(
       (acc, model) => {
         const date = moment(model.createdAt);
         const monthYear = date.format("MMMM YYYY");
@@ -32,7 +32,7 @@ export default function SearchBar() {
         acc[monthYear].push(model);
         return acc;
       },
-      {} as Record<string, typeof models.data.items>,
+      {} as Record<string, typeof models.data>,
     ) ?? {};
 
   const sortedMonthYears = Object.keys(groupedModels).sort(
@@ -44,7 +44,7 @@ export default function SearchBar() {
       immediate
       value={query}
       onChange={(value: string) => {
-        const selectedModel = models.data?.items.find((m) => m.name === value);
+        const selectedModel = models.data?.find((m) => m.name === value);
         if (selectedModel?.name) {
           router.push(`/models/${encodeURIComponent(selectedModel.name)}`);
         }
@@ -68,7 +68,7 @@ export default function SearchBar() {
         </div>
       </div>
       <ComboboxOptions className="absolute z-10 mt-1 max-h-48 w-full overflow-auto rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm md:max-h-40 lg:max-h-60">
-        {!models.data?.items.length ? (
+        {!models.data?.length ? (
           <div className="relative cursor-default select-none px-4 py-2">
             {models.isLoading ? "Loading..." : "No models found."}
           </div>
@@ -80,7 +80,7 @@ export default function SearchBar() {
               </div>
               {groupedModels[monthYear]?.map((model) => (
                 <ComboboxOption
-                  key={model.id}
+                  key={model.name}
                   value={model.name}
                   className="group flex cursor-pointer select-none items-center gap-2 bg-white px-4 py-2 hover:bg-blue-100"
                 >
