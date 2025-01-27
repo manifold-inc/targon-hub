76896c1857257094a556a2f4952961b4738ea5ea
Alan Cho <alancho397@gmail.com>
2025-01-27 16:10:18 -0600

    refactor: updated model attribute name

---
+++ src/app/api/models/stats/route.ts
@@ -0,0 +1,35 @@
+import { type NextRequest } from "next/server";
+import { asc, eq, inArray } from "drizzle-orm";
+import { z } from "zod";
+
+import { db } from "@/schema/db";
+import { DailyModelTokenCounts, Model } from "@/schema/schema";
+
+export async function POST(request: NextRequest): Promise<Response> {
+  try {
+    const { requestedModelNames } = z
+      .object({ requestedModelNames: z.array(z.string().min(1)) })
+      .parse(await request.json());
+
+    const stats = await db
+      .select({
+        modelName: DailyModelTokenCounts.modelName,
+        totalTokens: DailyModelTokenCounts.totalTokens,
+        avgTPS: DailyModelTokenCounts.avgTPS,
+        cpt: Model.cpt,
+      })
+      .from(DailyModelTokenCounts)
+      .leftJoin(Model, eq(DailyModelTokenCounts.modelName, Model.name))
+      .where(inArray(DailyModelTokenCounts.modelName, requestedModelNames))
+      .orderBy(asc(DailyModelTokenCounts.createdAt));
+    /**
+     * Average TPS
+     * Total Tokens
+     * Request Tokens
+     */
+    // iterate through list if multiple models
+    return Response.json({ stats: stats });
+  } catch (err) {
+    return Response.json({ error: "Invalid request", status: 400 });
+  }
+}
