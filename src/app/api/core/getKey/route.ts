663ead73aee4cbe5f3775087544d27d732dd98ea
Alan Cho <alancho397@gmail.com>
2025-01-27 19:48:52 -0600

    feat: add route for grabbing api keys for user

---
+++ src/app/api/core/getKey/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest } from "next/server";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+
+import { db } from "@/schema/db";
+import { ApiKey, User } from "@/schema/schema";
+
+export async function GET(request: NextRequest) {
+  try {
+    const auth = request.headers.get("Authorization");
+    const token = auth?.split(" ").at(-1);
+    if (!token) {
+      return Response.json({ error: "Unauthorized", status: 401 });
+    }
+    const [user] = await db
+      .select({ credits: User.credits, id: User.id })
+      .from(User)
+      .innerJoin(ApiKey, eq(User.id, ApiKey.userId))
+      .where(eq(ApiKey.key, token));
+    if (!user) {
+      return Response.json({ error: "Unauthorized", status: 401 });
+    }
+
+    const [apiKey] = await db
+      .select({ key: ApiKey.key })
+      .from(ApiKey)
+      .where(eq(ApiKey.userId, user.id));
+    if (!apiKey) {
+      return Response.json({ error: "User has no API keys", status: 401 });
+    }
+
+    return Response.json({ key: apiKey.key, status: 200 });
+  } catch (err) {
+    return Response.json({ error: "Unauthorized", status: 401 });
+  }
+}
