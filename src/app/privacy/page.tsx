36fa10a74bd23cb572a71c084826efb63a49023c
Sean Hershey <sean.p.hershey@gmail.com>
2025-02-03 09:33:55 -0600

    feat(privacy): privacy page with markdown

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,28 @@
+import fsPromises from "fs/promises";
+import path from "path";
+import matter from "gray-matter";
+import { marked } from "marked";
+
+export default async function Page() {
+  const filepath = path.join(
+    process.cwd(),
+    "src",
+    "app",
+    "privacy",
+    "privacy.md",
+  );
+  try {
+    const file = await fsPromises.readFile(filepath, "utf8");
+    const data = matter(file);
+
+    return (
+      <div
+        className={"prose !max-w-none p-16 text-sm dark:prose-invert"}
+        dangerouslySetInnerHTML={{ __html: marked(data.content) }}
+      />
+    );
+  } catch (error) {
+    console.error(error);
+    return <div>Error loading privacy policy</div>;
+  }
+}
